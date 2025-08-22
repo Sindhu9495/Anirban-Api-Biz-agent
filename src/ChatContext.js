a796c31a89f6b6d04dd74d6d852ab377acd74b7f
Sindhu9495 <sindhu.m@ceptes.com>
2025-08-22 12:34:36 +0530

    Update ChatContext.js

--- src/ChatContext.js
+++ src/ChatContext.js
@@ -1,17 +1,27 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState } from "react";
 
 export const ChatContext = createContext();
 
 export const ChatProvider = ({ children }) => {
-  const [messages, setMessages] = useState([]);
-  const [triggeredPrompt, setTriggeredPrompt] = useState(null);
+  const [chatState, setChatState] = useState({
+    isOpen: false,
+    isExpanded: false,
+    messages: [],
+  });
+
+  const [triggeredPrompt, setTriggeredPrompt] = useState("");
 
   const addMessage = (message) => {
-    setMessages((prev) => [...prev, message]);
+    setChatState((prev) => ({
+      ...prev,
+      messages: [...prev.messages, message],
+    }));
   };
 
   return (
-    <ChatContext.Provider value={{ messages, addMessage, triggeredPrompt, setTriggeredPrompt }}>
+    <ChatContext.Provider
+      value={{ chatState, setChatState, triggeredPrompt, setTriggeredPrompt, addMessage }}
+    >
       {children}
     </ChatContext.Provider>
   );
